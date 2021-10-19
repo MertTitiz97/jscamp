cbc5a955dac83bcad4dd62647c201bd4a3c6449d
MertTitiz97 <mertsen1997@hotmail.com>
2021-10-19 23:24:55 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,32 @@
+console.log("merhaba kodlama.io")
+//type safe degildir.
+let dolarDun = 9.20
+let dolarBugun = 9.30
+
+
+{
+    let dolarDun =9.10
+}
+
+
+console.log(dolarDun)
+
+const euroDun=11.2
+//euroDun =11
+
+console.log(euroDun)
+
+let konutKrediler =["konut kredileri","emlak konut kredisi","kamu konut kredisi"]
+for(let i=0; i<konutKrediler.length; i++){
+console.log(konutKrediler[i])
+}
+//let konutKredileri = ["konut Kredisi", "emlak konut kredisi","kamu konut kredisi"]
+//for(let i = 0; i<3; i++)
+
+//{
+    //console.log("<li>"+i+"<li>")
+//}
+
+
+
+
